6ad1cf38961486a28a3581d6519f3a2fa8045319
Rafael Pedroza <pedroza.oliveira@gmail.com>
2020-07-16 20:04:41 -0300

    Testando maniulação de strings com o JS

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,13 @@
-alert("Seja be vindo à Jamile Almeida MakeUp")
+var nome = "Rafael Pedroza";
+var Idade1 = 29;
+var Idade2 = 11;
+
+console.log(nome);
+console.log(Idade1+Idade2+Idade1)
+var frase = "O Conhecimento é infinito";
+
+console.log(frase.replace ("Conhecimento","feijão"));
+console.log(frase.toUpperCase(frase))
+
+// alert("Seja be vindo à Jamile Almeida MakeUp")
+// alert("Meu nome é: " + nome + " e minha idade: " + Idade);
