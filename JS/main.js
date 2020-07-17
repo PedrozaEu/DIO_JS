88a9c2262000eaeeeb46dde1b511c008b0cf8426
Rafael Pedroza <pedroza.oliveira@gmail.com>
2020-07-16 22:45:20 -0300

    Acrescentado o conceito de dicionario

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,26 @@
-var nome = "Rafael Pedroza";
+// Dicionário
+var carro = {nome: "Astra", ano: "2005", modelo: "Hatch", };//no dicionario os itens nao sao chamados por indices, mas pelo nome que pode ser um alias
+console.log(carro);
+console.log(carro.nome);
+console.log(carro.ano);
+
+
+
+/* var lista = ["Meia", "Calça", "Tenis", "Camisa", "Boné"];
+//lista.push("Sandália"); //Adiciona um item ao final da lista
+//lista.pop(); //Remove o ultimo item da lista 
+console.log(lista);//exibe a lista
+console.log(lista.length);//exibe o tamanho da lista
+console.log(lista.reverse());//exibe a lista do fim pro começo
+console.log(lista.sort());//exibe a lista em ordem alfabética
+console.log(lista[3]);//exibe o indice [n] da lista
+console.log(lista.toString());//exibe a lista em string (fora do array)
+console.log(lista.join(" / "));//Separa cada elemento da lista com o elemento indicado no join 
+
+
+
+
+/* var nome = "Rafael Pedroza";
 var Idade1 = 29;
 var Idade2 = 11;
 
@@ -11,3 +33,4 @@ console.log(frase.toUpperCase(frase))
 
 // alert("Seja be vindo à Jamile Almeida MakeUp")
 // alert("Meu nome é: " + nome + " e minha idade: " + Idade);
+ */ 
