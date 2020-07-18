a098e5e34426c8a21f713db4207dc1c83f8b2582
Rafael Pedroza <pedroza.oliveira@gmail.com>
2020-07-18 13:58:57 -0300

    Estudando functions - Usando soma e replace

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,17 @@
+//Funcions
+//soma
+var n1, n2; //var recebe 2 numeros atraves do prompt
+n1 = prompt ("Insira o 1º numero");//n1
+n2 = prompt ("Insira o 2º numero");//n2
+function soma (n1,n2) {
+    return n1+n2;
+}//a função "soma" realiza a ação mediante os parametrospassados;
+console.log(soma(parseInt(n1),parseInt(n2)));//para somar o que vm do prompt deve ser feito o parse na impressão, senao vira string;
+
+//exemplo: function replace
+
+
+/*
 //Datas
 var d = new Date();
 var data = d.getDate();
@@ -5,7 +19,7 @@ var mes = d.getMonth()+1;
 var ano = d.getFullYear();
 var lista = [data, mes, ano];
 console.log(lista.join("/"))
-
+console.log(d);
 
 
 /*
@@ -55,19 +69,14 @@ console.log(lista.toString());//exibe a lista em string (fora do array)
 console.log(lista.join(" / "));//Separa cada elemento da lista com o elemento indicado no join 
 
 
-
-
 /* var nome = "Rafael Pedroza";
 var Idade1 = 29;
 var Idade2 = 11;
-
 console.log(nome);
 console.log(Idade1+Idade2+Idade1)
 var frase = "O Conhecimento é infinito";
-
 console.log(frase.replace ("Conhecimento","feijão"));
 console.log(frase.toUpperCase(frase))
-
 // alert("Seja be vindo à Jamile Almeida MakeUp")
 // alert("Meu nome é: " + nome + " e minha idade: " + Idade);
  */ 
